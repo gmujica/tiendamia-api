7aa6aba9c25cc01c0ad904d0f2df4b6f46bdc47a
gmujica <gregorymujica1993@gmail.com>
2023-09-29 11:39:30 -0300

    update and delete services added

--- src/item/application/item/item.service.ts
+++ src/item/application/item/item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from '../../infrastructure/entity/item.entiy';
 import { Repository } from 'typeorm';
@@ -27,5 +27,22 @@ export class ItemService {
     const newItem = this.itemRepository.create(item);
     const savedItem = await this.itemRepository.save(newItem);
     return await this.itemRepository.save(savedItem);
+  }
+  //update item
+  async updateEvent(item_id: string, updatedItem: Item): Promise<Item> {
+    const existingItem = await this.findOne(item_id);
+    if (!existingItem) {
+      throw new NotFoundException(`Item with ID ${item_id} not found.`);
+    }
+    const mergedItem = this.itemRepository.merge(existingItem, updatedItem);
+    return await this.itemRepository.save(mergedItem);
+  }
+  //delete event
+  async deleteEvent(item_id: string): Promise<void> {
+    const existingItem = await this.findOne(item_id);
+    if (!existingItem) {
+      throw new NotFoundException(`Item with ID ${item_id} not found.`);
+    }
+    await this.itemRepository.remove(existingItem);
   }
 }
